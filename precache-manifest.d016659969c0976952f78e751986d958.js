b64758618e740d4d0bf22ace4793a88cc5306d6a
Maksim <max.ru6@mail.ru>
2020-09-29 12:03:02 +0400

    init

---
+++ precache-manifest.d016659969c0976952f78e751986d958.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9fd225fafa39de700a3",
+    "url": "/css/app.8ca275f9.css"
+  },
+  {
+    "revision": "e9163cc74195f5dc6dde",
+    "url": "/css/chunk-vendors.0c931200.css"
+  },
+  {
+    "revision": "0f2ca41a2be0fe2fbb5a129e7380aa16",
+    "url": "/img/Screenshot_1.png"
+  },
+  {
+    "revision": "47e86807878f1cbb95b70a0b6f893302",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9fd225fafa39de700a3",
+    "url": "/js/app.719943db.js"
+  },
+  {
+    "revision": "e9163cc74195f5dc6dde",
+    "url": "/js/chunk-vendors.15219f4f.js"
+  },
+  {
+    "revision": "624c3b1c4db9c467cd853d1fb76544ac",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
